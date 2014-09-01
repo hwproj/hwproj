2b67a667503a39f11ee21fe1ff3731d50c2003b6
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-09-01 16:29:13 +0400

    Add links to problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,6 +2,8 @@ class Problem < ActiveRecord::Base
   validates :text, presence: true
   belongs_to :homework
   has_many :tasks, dependent: :destroy
+  has_many :links, as: :parent, dependent: :destroy
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   def name
       "#{homework.number}.#{number}"
