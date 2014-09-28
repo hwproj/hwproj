296d1892422a570214746655ef000f5cb8a7e222
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-09-28 15:31:23 +0400

    Deny copies of a job

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@ class Job < ActiveRecord::Base
 	belongs_to :student, foreign_key: "user_id"
   belongs_to :homework
   has_many :tasks
+  validates_uniqueness_of :user_id, :scope => :homework_id
 end
