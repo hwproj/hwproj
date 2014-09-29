b6953503c11c8dda2472af6b51258fb912e04f42
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-09-29 10:54:22 +0400

    Destroy awards when destroy a homework

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ActiveRecord::Base
 	belongs_to :student, foreign_key: "user_id"
   belongs_to :homework
   has_many :tasks
-  has_many :awards
+  has_many :awards, dependent: :destroy
 end
