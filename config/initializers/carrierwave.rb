f5a600ee16fd059fee34fa368d6d1e108705a6a4
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-07 19:43:33 +0400

    Fix S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,6 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'eu-west-1'
     }
     config.fog_directory = ENV["AWS_S3_BUCKET"]
   end
