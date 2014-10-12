9359313f962ebb77e951524bd8287a734ecfd554
Lada <lada.gagina@gmail.com>
2014-10-13 02:53:46 +0400

    Fix submission adding

--- app/helpers/add_protocol.rb
+++ app/helpers/add_protocol.rb
@@ -1,6 +1,6 @@
 module AddProtocol
   def add_protocol
-    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//] || self.url.strip.empty?
       self.url = "http://#{self.url}"
     end
   end
