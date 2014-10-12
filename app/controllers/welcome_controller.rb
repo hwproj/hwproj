883b8f917527c897839e3b5244e3a1bd31e08636
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-12 23:54:55 +0400

    Update teacher feed for new db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,16 @@
 class WelcomeController < ApplicationController
   def index
-  	@submissions = Submission.paginate(page: params[:page])
-  	@groups = Group.all
+    @courses = Course.all
 
-  	if signed_in? && current_user.student?
-  		@student_feed = current_user.student_feed.paginate(page: params[:page])
-  		@deadline_tasks = current_user.deadline_tasks
-  		@overdue_tasks = current_user.overdue_tasks
-  	end
+    if signed_in?
+      if current_user.student?
+        @student_feed = current_user.student_feed.paginate(page: params[:page])
+        @deadline_tasks = current_user.deadline_tasks
+        @overdue_tasks = current_user.overdue_tasks
 
+      elsif current_user.teacher?
+        @submissions = Submission.where(teacher_id: current_user.id).paginate(page: params[:page])
+      end
+    end
   end
 end
