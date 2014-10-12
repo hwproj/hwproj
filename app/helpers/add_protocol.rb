5e8db456f22366250403f48c4fa8e017986d1ed0
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-13 03:30:50 +0400

    Fix 'add_protocol' callback for submissions

--- app/helpers/add_protocol.rb
+++ app/helpers/add_protocol.rb
@@ -1,7 +1,9 @@
 module AddProtocol
   def add_protocol
-    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//] || self.url.strip.empty?
-      self.url = "http://#{self.url}"
+    unless self.url.nil? || self.url.strip.empty?
+      unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//] || self.url.strip.empty?
+        self.url = "http://#{self.url}"
+      end  
     end
   end
 end
