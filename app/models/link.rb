f0106fa59426d91a388f534fdccccd35f2241d9b
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-12 22:50:36 +0400

    Prepend protocol in URIs

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,10 @@
 class Link < ActiveRecord::Base
+  before_save :add_protocol
   belongs_to :parent, polymorphic: true
+
+  def add_protocol
+    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+      self.url = "http://#{self.url}"
+    end
+  end
 end
