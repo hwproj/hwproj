7c8a895c575790f827381627171e499dca5eebba
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-15 17:13:07 +0400

    Preserve simple formatting in problems' texts

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,15 @@
 class Problem < ActiveRecord::Base
   validates :text, presence: true
+  before_save :clean_name
+
   belongs_to :homework
+
   has_many :tasks, dependent: :destroy
   has_many :links, as: :parent, dependent: :destroy
+
   accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
+
+  def clean_name
+    self.name = nil if self.name.blank?
+  end
 end
