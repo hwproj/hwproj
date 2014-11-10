0020e39821640e7b8df777c4398994f745a6b350
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-11-10 13:31:19 +0300

    Better tasks sorting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109163634) do
+ActiveRecord::Schema.define(version: 20141110102641) do
 
   create_table "awards", force: true do |t|
     t.integer  "job_id"
@@ -116,7 +116,6 @@ ActiveRecord::Schema.define(version: 20141109163634) do
     t.integer  "homework_id"
     t.integer  "job_id"
     t.integer  "student_id"
-    t.integer  "number"
   end
 
   add_index "tasks", ["job_id"], name: "index_tasks_on_job_id"
