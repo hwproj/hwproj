9ec882c81a28205e688d4a720d08935bf40ccaee
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2015-03-17 10:57:39 +0300

    Better approve cancelling

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def after_cancel_status(task)
+    if task.notes.any?
+      "accepted_partially"
+    elsif task.submissions.any?
+      "not_accepted"
+    else
+      "not_submitted"
+    end
+  end
 end
