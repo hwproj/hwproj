5fbce41a137117a6a3634bb4ad2218d4a1f32b7b
kbatoev <kbatoev@mail.ru>
2016-05-01 11:16:19 +0300

    Now teacher can see number of tasks to check

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,29 @@ class WelcomeController < ApplicationController
 
       elsif current_user.teacher?
         @submissions = Submission.where(teacher_id: current_user.id).paginate(page: params[:page])
+        @numberOfTasks = 0
+        
+        @courses = current_user.courses
+
+        @tasks_left_all = 0
+        @tasks_left_of_last_terms = 0
+
+        @courses.each do |course|
+          course.terms.each do |term|
+            students = term.students.where(approved: true).includes(jobs: :tasks)
+            students.each do |student|
+              @tasks_left_all += student.tasks_left_count
+            end
+          end
+
+          course.terms.last.students.where(approved: true).includes(jobs: :tasks).each do |student|
+            @tasks_left_of_last_terms += student.tasks_left_count
+          end
+
+        end
+
       end
     end
   end
+
 end
