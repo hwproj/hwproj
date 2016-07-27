369747506e9b5844b03693161f1fda3334a37107
Viola <anstkras@gmail.com>
2016-07-27 18:34:00 +0300

    setted host variable for development and testing

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Hwproj
       generate.assets false
       generate.view_specs false
     end
+    unless Rails.env.production?
+      ENV['HOST'] = 'hwproj.me'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
