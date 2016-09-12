b72cca2bfaa8093c63ac379ca0fec9970c0069e4
kbatoev <kbatoev@mail.ru>
2016-09-12 11:36:19 +0300

    Improved design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar img-rounded")
   end
 end
