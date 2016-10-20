6c3e5d41391b7402c278eaed1262a4dd8d592099
Anastasiya Krasnoryadtseva <anstkras@gmail.com>
2016-10-20 21:51:07 +0300

    Now webhooks are creating correctly
    Now it checks if there is a webhook with old URL and updates that webhook. Also it checks if there is a webhook created by this application instead of checking if there are any webhooks.

--- app/helpers/github.rb
+++ app/helpers/github.rb
@@ -6,10 +6,15 @@ module Github
     client = Octokit::Client.new access_token: current_user.github_access_token
     hooks = client.hooks(repo_full_name)
 
-    if !hooks || hooks.empty? || !hooks.map{|x| x.events}.any?{|e| e.include? "pull_request"}
+    old_url = 'https://hwproj.herokuapp.com/github/payload'
+    urls = hooks.map{|x| x.config.url} if hooks && hooks.any?
+
+    if !hooks || hooks.empty? ||
+        (!urls.include?("#{ENV["HOST"]}/github/payload") &&
+            !urls.include?(old_url))
       client.create_hook(repo_full_name, 'web',
       {
-        url: "#{ENV["CURRENT_HOST"]}/github/payload",
+        url: "#{ENV["HOST"]}/github/payload",
         secret: ENV["GITHUB_WEBHOOK_SECRET"],
         content_type: 'json'
       },
@@ -17,6 +22,21 @@ module Github
         events: ['pull_request', 'pull_request_review_comment', 'issue_comment'],
         active: true
       })
+    elsif urls && urls.include?(old_url)
+      hooks.each do |h|
+        if h.config.url == old_url
+          client.edit_hook(repo_full_name, h.id, 'web',
+          {
+           url: "#{ENV["HOST"]}/github/payload",
+           secret: ENV["GITHUB_WEBHOOK_SECRET"],
+           content_type: 'json'
+          },
+          {
+           events: ['pull_request', 'pull_request_review_comment', 'issue_comment'],
+           active: true
+          })
+        end
+      end
     end
   end
 end
