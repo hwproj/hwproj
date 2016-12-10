e3ade075a81a2f463397fe7d37615ca7f7ce142e
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2016-12-10 11:45:25 +0300

    Added scrollbar for table and prevented multiline displaying of the row

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -9,6 +9,10 @@ class Problem < ActiveRecord::Base
 
   accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
+  def get_displaying_name
+    name ? name : number
+  end
+
   private
     def set_number
       self.number = self.homework.problems.count + 1
