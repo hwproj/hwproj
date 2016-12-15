f68804de3244e1189de83dbcc0b34f5d6e85e3e1
Afletunova <afletunova.yulia@gmail.com>
2016-12-15 20:09:20 +0300

    Add login support via Google

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,12 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+	def google_oauth2
+       @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+        sign_in_and_redirect @user, :event => :authentication
+      end
+  end
+end
