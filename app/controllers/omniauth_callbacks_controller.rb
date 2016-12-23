7b8a4cf9bf1d372b1adb2859e796bdc2081c966c
Afletunova <afletunova.yulia@gmail.com>
2016-12-23 08:33:12 +0300

    Rewrite names

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,6 @@
 class OmniauthCallbackController < Devise::OmniauthCallbackController
 	def google_oauth2
-
-		@useer = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
+		@user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 		if @user.persisted?
 			flash[ :notice ] = I18n.t "devise.omniauth_callbacks.success", kind: 'Google'
 			sign_in_and_redirect @user, :event => :authentication
