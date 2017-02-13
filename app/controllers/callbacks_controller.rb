3bad9ff371907e5c6ed366fe91d1f0a98a99a925
Afletunova <afletunova.yulia@gmail.com>
2017-02-13 19:19:06 +0300

    Fix: correct output of Google auth, format omniauth_callbacks_controller, rewrite callbacks_controller to a new syntax

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,10 +3,10 @@ class CallbacksController < Devise::OmniauthCallbacksController
        @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
+        sign_in_and_redirect @user, event: :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
-        sign_in_and_redirect @user, :event => :authentication
+        sign_in_and_redirect @user, event: :authentication
       end
   end
 end
