995c574ebb77717a0ed9da73316d7f85f820f6d6
Sergey Konev <sergey@cimon.io>
2017-10-24 03:51:24 +0300

    Fix NotificationsController

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,23 @@
 class NotificationsController < ApplicationController
-	respond_to :js
-	def update
-		@notification = Notification.find(params[:id])
-		@notification.is_read = params[:is_read]
-		@notification.save!
-		render nothing: true
-	end
+  respond_to :js
+  helper_method :resource
+
+  def update
+    update_resource
+    respond_with resource
+  end
+
+  private
+
+  def update_resource
+    resource.update!(permitted_params)
+  end
+
+  def resource
+    @resource ||= current_user.notifications.find(params[:id])
+  end
+
+  def permitted_params
+    params.require(:notification).permit(:is_read)
+  end
 end
