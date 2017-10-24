c0d32ca1b0c767c1fed6cb2974e8fd2856b85588
Sergey Konev <sergey@cimon.io>
2017-10-24 03:52:47 +0300

    Add dom helpers; update views with unobtrusive js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar img-rounded")
   end
+
+  def dom_id(*args)
+    args.map do |a|
+      if a.respond_to?(:id)
+        "#{dom_class(a)}_#{a.id}"
+      elsif a.respond_to?(:to_sym)
+        a.to_sym
+      elsif a.respond_to?(:to_key)
+        a.to_key
+      else
+        a.object_id
+      end
+    end.join('-')
+  end
+
+  def css_id(*args)
+    "##{dom_id(*args)}"
+  end
 end
